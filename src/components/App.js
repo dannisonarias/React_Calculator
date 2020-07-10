0c2aa9203e7b651cae1a8f88c7477335986f8f3e
DannisonArias <dannisonarias@gmail.com>
2020-07-10 16:51:13 -0400

    fix App height & width

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
-const App = () => (
-  <div id="calculator">
-    <Display />
-    <ButtonPanel />
-  </div>
-);
+const App = () => {
+  const styles = {
+    width: '700px',
+    height: '100%',
+  };
+
+  return (
+    <div style={styles} id="calculator">
+      <Display />
+      <ButtonPanel />
+    </div>
+  );
+};
 export default App;
